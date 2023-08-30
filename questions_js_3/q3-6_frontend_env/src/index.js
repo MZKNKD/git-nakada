457625a7b479395080af82df017ffacc278d5c13
mzknkd <mzk.nkd@gmail.com>
2023-08-30 16:48:29 +0900

    Q3-6　フロントエンド環境構築をしました

---
+++ questions_js_3/q3-6_frontend_env/src/index.js
@@ -0,0 +1,13 @@
+import './style.scss';
+require('intersection-observer');
+import 'whatwg-fetch';
+
+const moment = require('moment'); // require
+console.log(moment().format());
+
+
+console.log('test');
+
+window,addEventListener('load', ()=>{
+    alert('babelテスト');
+})
