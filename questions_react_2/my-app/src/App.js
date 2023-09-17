b2a5c5d17326c3b867aa86d444e5d489d3ce7685
mzknkd <mzk.nkd@gmail.com>
2023-09-18 02:41:04 +0900

    Q6を再提出いたします

--- questions_react_2/my-app/src/App.js
+++ questions_react_2/my-app/src/App.js
@@ -1,14 +1,63 @@
+// App.js
 import logo from './logo.svg';
 import './App.css';
 import FormComponent from './FormComponent';
 import PostalCode from './PostalCode';
-import React, {useState} from 'react'
-
+import React, { useState } from 'react';
 
 function App() {
+  const [postalCode, setPostalCode] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [address, setAddress] = useState('');
+
+  const zipcloudURL = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=";
+  const options = {
+    method: 'GET'
+  };
+
+  const handleChange = (newPostalCode) => {
+    setPostalCode(newPostalCode);
+  };
+
+  const handleClick = async e => {
+    e.preventDefault()
+    setErrorMessage(''); // Reset error message
+    setAddress(''); // Reset address
+
+    if (/^\d{7}$/.test(postalCode)) {  //.test:合致しているかどうかを検証し、結果として true を返す
+      const searchURL = zipcloudURL + postalCode;
+      console.log(searchURL);
+
+      try {
+        const response = await fetch(searchURL, options);
+        const response2 = await response.json();
+
+        if (response2.results === null) {
+          setErrorMessage('該当する住所が見つかりませんでした');
+        } else {
+          console.log(response2.results[0].address1);
+          const fullAddress = response2.results[0].address1 + response2.results[0].address2 + response2.results[0].address3;
+          console.log(fullAddress);
+          setAddress(fullAddress);
+        }
+      } catch (e) {
+        console.log("エラー：", e.message);
+        //setErrorMessage('エラーが発生しました');
+      }
+    } else {
+      setErrorMessage('7桁の数字をハイフン無しで入力してください');
+    }
+  };
+
   return (
     <div className="App">
-      <GetAddress/>
+      <PostalCode
+        postalCode={postalCode}
+        handleChange={handleChange}
+        handleClick={handleClick}
+        errorMessage={errorMessage}
+        address={address}
+      />
       <FormComponent />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -26,76 +75,6 @@ function App() {
       </header>
     </div>
   );
-}
-
-const zipcloudURL = "https://zipcloud.ibsnet.co.jp/api/search?zipcode="
-
-const options = {
-  method: 'GET'
-};
-
-export const GetAddress = () => {
-  const [postalCode, setPostalCode ] = useState({
-    zip: ''
-  })
-
-  const handleChange = e =>{
-    const {name, value} = e.target
-    setPostalCode({
-        ...postalCode,
-        [name]: value,
-    })
-  }
-
-  const handleClick = e => {
-    e.preventDefault()
-
-    const errorMessage = document.getElementById("error_message");
-    let addressInput = document.getElementById("address")
-
-    if(postalCode.zip.length === 7){
-      const searchURL = zipcloudURL + postalCode.zip;
-      console.log(searchURL);
-      errorMessage.classList.add('red', 'remove');
-
-      function getPostCode(searchURL, options){
-          return fetch(searchURL, options)
-          .then( response => response.json())
-                  .then( response2 => {
-                      if(response2.results === null){
-                          errorMessage.classList.remove('remove');
-                          errorMessage.textContent = '住所の検索ができませんでした';
-                          addressInput.value = null
-                      };
-
-                      console.log(response2.results[0].address1);
-                      var fullAddress = response2.results[0].address1 + response2.results[0].address2 + response2.results[0].address3;
-                      console.log(fullAddress)
-                      addressInput.value = fullAddress;
-
-                  }).catch(e=>{
-                      console.log("エラー：", e.message);
-              });
-          }
-      
-      async function getCode(searchURL, options){
-          const response = await getPostCode(searchURL, options);
-      }
-
-      getCode(searchURL, options);
-    } else {
-        errorMessage.textContent = "7桁の数字をハイフン無しで入力してください";
-        errorMessage.classList.remove('remove');
-        errorMessage.classList.add('red')
-        addressInput.value = null
-    }
-  }
-
-  return(
-    <>
-      <PostalCode handleClick={handleClick} handleChange={handleChange}/>
-    </>
-  )
 }
 
 export default App;
